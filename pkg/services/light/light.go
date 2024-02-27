18fb1df35b9711b56780740eaa05bdcc7c4df520
Jonathan Mataloni <jo.mataloni@gmail.com>
2024-02-27 23:32:33 +0100

    stdize light service

--- pkg/services/light/light.go
+++ pkg/services/light/light.go
@@ -3,15 +3,15 @@ package lights
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/dgraph-io/badger/v4"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jonamat/makai-automations/pkg/utils"
 )
 
-const AUTOMATION_TOPIC = "aut/light"
+const ENABLED_TOPIC = "aut/light/enabled"
 const LIGHT_LEVEL_TOPIC = "aut/light/level"
-const LIGHT_LEVEL_SET_TOPIC = "aut/light/level/set"
 
 const DEFAULT_ENABLED = true
 const DEFAULT_LIGHT_LEVEL = 255
@@ -29,34 +29,40 @@ func StartService() {
 	dbClient = utils.CreateDbClient()
 	defer dbClient.Close()
 
+	for !mqttClient.IsConnected() {
+		fmt.Println("Waiting for mqtt connection...")
+		time.Sleep(1 * time.Second)
+	}
+
 	setupDb(dbClient)
 
-	mqttClient.Subscribe(AUTOMATION_TOPIC, 0, func(client mqtt.Client, msg mqtt.Message) {
+	mqttClient.Subscribe(ENABLED_TOPIC+"/set", 0, func(client mqtt.Client, msg mqtt.Message) {
 		fmt.Println("Received message on topic: ", msg.Topic(), " with payload: ", string(msg.Payload()))
 
 		switch string(msg.Payload()) {
 		case "ON":
 			setEnabled(true)
-			enable(mqttClient)
+			enable()
+			mqttClient.Publish(ENABLED_TOPIC, 0, false, "ON")
 		case "OFF":
 			setEnabled(false)
 			disable(mqttClient)
+			mqttClient.Publish(ENABLED_TOPIC, 0, false, "OFF")
 		}
 	})
 
 	// first start
 	enabled, _ := getEnabled()
 	if enabled {
-		enable(mqttClient)
+		enable()
 	}
 
-	fmt.Println("Listening for messages on dev/cabin-pir")
 	select {}
 }
 
-func enable(mqttClient mqtt.Client) {
+func enable() {
 	// getters and setters
-	mqttClient.Subscribe(LIGHT_LEVEL_SET_TOPIC, 0, func(client mqtt.Client, msg mqtt.Message) {
+	mqttClient.Subscribe(LIGHT_LEVEL_TOPIC+"/set", 0, func(client mqtt.Client, msg mqtt.Message) {
 		fmt.Println("Received message on topic: ", msg.Topic(), " with payload: ", string(msg.Payload()))
 
 		level, err := strconv.Atoi(string(msg.Payload()))
@@ -97,6 +103,6 @@ func disable(mqttClient mqtt.Client) {
 	mqttClient.Publish("dev/main-light/set", 0, false, fmt.Sprintf("SET %d", 0))
 
 	// unsubscribtions
-	mqttClient.Unsubscribe("dev/cabin-pir")
-	mqttClient.Unsubscribe(LIGHT_LEVEL_TOPIC)
+	mqttClient.Unsubscribe(ENABLED_TOPIC + "/set")
+	mqttClient.Unsubscribe(LIGHT_LEVEL_TOPIC + "/set")
 }
